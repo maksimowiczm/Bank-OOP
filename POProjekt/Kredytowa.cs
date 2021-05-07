89337832e42359c7f9629936e02d9da96c36d7c5
matmak13 <maksimowiczmateusz13@gmail.com>
2021-05-07 12:52:26 +0200

    Kredytowa.cs

---
+++ POProjekt/Kredytowa.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POProjekt
+{
+    public class Kredytowa : Debetowa
+    {
+        public readonly decimal MaksymalnyKredyt;
+        public decimal AktualnyKredyt
+        {
+            get
+            {
+                if (saldo >= 0)
+                    return 0;
+                else
+                {
+                    return Math.Abs(Saldo);
+                }
+            }
+        }
+
+        public Kredytowa(int idBanku, int idKlienta, decimal kredyt)
+            :this(idBanku, idKlienta, kredyt, 0) { }
+        public Kredytowa(int idBanku, int idKlienta, decimal kredyt, decimal saldo)
+            :base(idBanku, idKlienta, 0)
+        {
+            if (kredyt <= 0)
+                throw new Exception("Niedodatni kredyt");
+            if (saldo < -kredyt)
+                throw new Exception("Za niskie saldo");
+            this.saldo = saldo;
+            MaksymalnyKredyt = kredyt;
+        }
+        public Kredytowa(int idBanku, int idKlienta, decimal kredyt, decimal saldo, string num)
+            :base(idBanku, idKlienta, 0, num)
+        {
+            if (kredyt <= 0)
+                throw new Exception("Niedodatni kredyt");
+            if (saldo < -kredyt)
+                throw new Exception("Za niskie saldo");
+            this.saldo = saldo;
+            MaksymalnyKredyt = kredyt;
+        }
+
+        public override bool Wyplac(decimal kwota)
+        {
+            if (!zweryfikujKwote(kwota))
+                return false;
+
+            if (saldo - kwota >= -MaksymalnyKredyt)
+            {
+                saldo -= kwota;
+                return true;
+            }
+            else
+                return false;
+
+        }
+    }
+}
