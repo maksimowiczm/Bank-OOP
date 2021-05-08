4d6cdf46e9e92304da5ed027d640e971fca64a44
TheRavvenKlou <klaudia134000@wp.pl>
2021-05-08 19:23:26 +0200

    Klient normalny

--- POProjekt/Klient.cs
+++ POProjekt/Klient.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace POProjekt
 {
-    class Klient
+    public class Klient
     {
+        private static int ilosc;
+        public readonly int Id;
+        public readonly string Imie;
+        public readonly string Nazwisko;
+        private List<Karta> karty;
+        public Klient(string imie, string nazwisko, int id)
+        {
+            if (id < 0) throw new Exception("Id ujemne");
+            this.Imie = imie;
+            this.Nazwisko = nazwisko;
+            this.Id = id;
+            ilosc++;
+        }
+        public Klient(string imie, string nazwisko) : this(imie, nazwisko, ilosc) {}
+
+
+
+        
     }
 }
