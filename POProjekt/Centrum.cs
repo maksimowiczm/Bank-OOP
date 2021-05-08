f19331dbb3e3114275a3979d788109a2b8284ac9
matmak13 <maksimowiczmateusz13@gmail.com>
2021-05-08 22:40:15 +0200

    ZapiszTransakcje

--- POProjekt/Centrum.cs
+++ POProjekt/Centrum.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Newtonsoft.Json;
 
 namespace POProjekt
 {
     public static class Centrum
     {
-        private static List<Transakcja> transakcje;
+        private static List<Transakcja> transakcje = new List<Transakcja>();
         public static List<Transakcja> Transakcje{ get => transakcje; }
 
         public static bool AutoryzujTransakcje(Firma firma, Karta karta, decimal kwota)
@@ -19,6 +20,11 @@ namespace POProjekt
             var transakcja = new Transakcja(DateTime.Now, sukces, bank.Id, bank.Nazwa, karta.IdKlienta, karta.Numer, kwota);
             transakcje.Add(transakcja);
             return sukces;
+        }
+        public static void ZapiszTransakcje(string uri)
+        {
+            var json = JsonConvert.SerializeObject(transakcje, Formatting.Indented);
+            File.WriteAllText($"{uri}.json", json);
         }
     }
 }
