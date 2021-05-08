de0abd88410c8a486aae5bafbd207ae167a9ae75
matmak13 <maksimowiczmateusz13@gmail.com>
2021-05-08 22:26:18 +0200

    Centrum.cs

---
+++ POProjekt/Centrum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace POProjekt
+{
+    public static class Centrum
+    {
+        private static List<Transakcja> transakcje;
+        public static List<Transakcja> Transakcje{ get => transakcje; }
+
+        public static bool AutoryzujTransakcje(Firma firma, Karta karta, decimal kwota)
+        {
+            var bank = Bank.GetBank(karta.IdBanku);
+            if (bank == null)
+                return false;
+
+            var sukces = bank.RealizujTransakcje(karta, kwota);
+            var transakcja = new Transakcja(DateTime.Now, sukces, bank.Id, bank.Nazwa, karta.IdKlienta, karta.Numer, kwota);
+            transakcje.Add(transakcja);
+            return sukces;
+        }
+    }
+}
