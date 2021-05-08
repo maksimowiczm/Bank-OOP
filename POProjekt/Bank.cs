82e01f34d665805ec4041df0b9ff511c13c715d3
TheRavvenKlou <klaudia134000@wp.pl>
2021-05-08 19:38:04 +0200

    Bank here

---
+++ POProjekt/Bank.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POProjekt
+{
+	public class Bank
+	{
+		private static int ilosc;
+		private static List<Bank> banki;
+		public readonly int Id;
+		public readonly string Nazwa;
+		private List<Karta> karty;
+
+		static Bank GetBank(int id)
+		{
+			return banki.Find(b => b.Id == id); 
+		}
+        public Bank(string nazwa, int id, List<Karta> karty)
+        {
+			if (id < 0) throw new Exception("Id ujemne");
+			this.Nazwa = nazwa;
+			this.Id = id;
+			this.karty = karty;
+			ilosc++;
+        }
+		public Bank(string nazwa) : this(nazwa, ilosc, new List<Karta>()) { }
+
+        
+       
+	}
+}
