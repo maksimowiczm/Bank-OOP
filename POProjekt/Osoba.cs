08c93a995b1af4995e5418e3e7ff173776dde79b
TheRavvenKlou <klaudia134000@wp.pl>
2021-05-21 20:56:51 +0200

    Osoba.cs

---
+++ POProjekt/Osoba.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace POProjekt
+{
+    public class Osoba : Klient
+    {
+        public readonly string Imie;
+        public readonly string Nazwisko;
+        private List<Karta> karty;
+        public List<Karta> Karty { get => new List<Karta>(karty); }
+
+        [JsonConstructor]
+        public Osoba(string imie, string nazwisko, List<Karta> karty)
+        {
+            this.Imie = imie;
+            this.Nazwisko = nazwisko;
+            this.karty = karty;
+        }
+        public Osoba(string imie, string nazwisko) : this(imie, nazwisko, new List<Karta>()) { }
+
+        public bool DodajKarteDebetowa(Konto konto)
+        {
+            throw new NotImplementedException();
+        }
+        public bool DodajKarteKredytowa(Bank bank, decimal maksymalnyKredyt, decimal saldo)
+        {
+            if (maksymalnyKredyt < 0) return false;
+            if (saldo < -maksymalnyKredyt) return false;
+            throw new NotImplementedException();
+        }
+        public bool DodajKarteKredytowa(Bank bank, decimal maksymalnyKredyt) => DodajKarteKredytowa(bank, maksymalnyKredyt, 0);
+        private int mojaKarta(Karta karta) => karty.IndexOf(karta);
+        public bool UsunKarte(Karta karta)
+        {
+            if (mojaKarta(karta) < 0) return false;
+            karty.Remove(karta);
+            return true;
+        }
+    }
+}
