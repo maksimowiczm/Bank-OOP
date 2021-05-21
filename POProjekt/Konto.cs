526eb9082acf47a6ea0e12dadf0972369e7fb92a
TheRavvenKlou <klaudia134000@wp.pl>
2021-05-21 20:40:37 +0200

    konto cs

--- POProjekt/Konto.cs
+++ POProjekt/Konto.cs
@@ -1,27 +1,17 @@
-﻿using System;
-
-namespace POProjekt
+﻿namespace POProjekt
 {
     public class Konto
     {
-        public readonly int idBanku;
-        public readonly int idFirmy;
+        public readonly Bank Bank;
+        public readonly Klient Klient;
         private decimal saldo;
-        public decimal Saldo { get => saldo; }
 
-        public Konto(int idBanku, int idFirmy, decimal saldo)
+        public Konto(Bank bank, Klient klient) : this(bank, klient, 0) { }
+        public Konto(Bank bank, Klient klient, decimal saldo)
         {
-            this.idBanku = idBanku;
-            this.idFirmy = idFirmy;
+            Klient = klient;
+            Bank = bank;
             this.saldo = saldo;
-        }
-        public Konto(int idBanku, int idFirmy) : this(idBanku, idFirmy, 0) { }
-
-        public void Wplac(decimal kwota)
-        {
-            if (kwota <= 0)
-                throw new Exception("Niedodatnia kwota");
-            saldo += kwota;
         }
     }
 }
