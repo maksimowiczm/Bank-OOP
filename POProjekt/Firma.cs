31e8fbce581afadc732d6f0146fbba1bc25c7e08
TheRavvenKlou <klaudia134000@wp.pl>
2021-05-21 21:02:34 +0200

    Firma.cs

--- POProjekt/Firma.cs
+++ POProjekt/Firma.cs
@@ -1,33 +1,23 @@
-﻿namespace POProjekt
-{
-    public class Firma
-    {
-        private static int ilosc;
-        public readonly string Nazwa;
-        public readonly string Kategoria;
-        public readonly int Id;
-        public Konto Konto { get; }
+﻿using System;
 
-        public Firma(string nazwa, string kategoria, Konto konto, int id)
-        {
-            Id = id;
-            ilosc++;
-            Nazwa = nazwa;
-            Kategoria = kategoria;
-            this.Konto = konto;
+namespace POProjekt
+{
+	public class Firma: Klient
+	{
+		public readonly string Nazwa;
+		public readonly string Kategoria;
+		public readonly Centrum Centrum;
+
+		public Firma(string nazwa, string kategoria,Centrum centrum)
+		{
+			Nazwa = nazwa;
+			Kategoria = kategoria;
+            Centrum = centrum;
         }
-        public Firma(string nazwa, string kategoria, Bank bank) : this(nazwa, kategoria, bank.StworzKonto(ilosc), ilosc) { }
-        public Firma(string nazwa, string kategoria, Bank bank, decimal saldo) : this(nazwa, kategoria, bank.StworzKonto(ilosc, saldo), ilosc) { }
-        public bool PoprosOAutoryzacje(Karta karta, decimal kwota)
+
+        public bool PoprosOAutoryzacje(Karta karta, decimal saldo)
         {
-            if (kwota > 0)
-            {
-                var sukces = Centrum.AutoryzujTransakcje(this, karta, kwota);
-                if (sukces)
-                    Konto.Wplac(kwota);
-                return sukces;
-            }
-            else return false;
+            throw new NotImplementedException();
         }
     }
 }
