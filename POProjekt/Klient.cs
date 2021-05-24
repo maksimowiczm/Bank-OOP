5ad06c6db47aea84ab39fe42e41b6b03d93f4863
matmak13 <maksimowiczmateusz13@gmail.com>
2021-05-24 11:25:30 +0200

    Klient.cs

--- POProjekt/Klient.cs
+++ POProjekt/Klient.cs
@@ -6,5 +6,21 @@ namespace POProjekt
     {
         protected List<Konto> konta;
         public IList<Konto> Konta => konta.AsReadOnly();
+
+        public bool DodajKonto(Konto konto)
+        {
+            if (konto.Klient != this || konta.Contains(konto)) return false;
+
+            konta.Add(konto);
+            return true;
+        }
+
+        public bool UsunKonto(Konto konto)
+        {
+            if (konto.Klient != this || konta.Contains(konto)) return false;
+
+            konta.Remove(konto);
+            return true;
+        }
     }
 }
