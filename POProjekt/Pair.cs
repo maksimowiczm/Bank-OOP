93abe43a7806237a05a8bf956729e617416cf0b7
matmak13 <maksimowiczmateusz13@gmail.com>
2021-05-25 21:43:48 +0200

    klasa Pair

---
+++ POProjekt/Pair.cs
@@ -0,0 +1,14 @@
+﻿namespace POProjekt
+{
+    public class Pair
+    {
+        public readonly string Andor;
+        public readonly object Obj;
+
+        public Pair(string andor, object obj)
+        {
+            Andor = andor;
+            Obj = obj;
+        }
+    }
+}
