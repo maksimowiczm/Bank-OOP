65c972dfb687d693f6cc095c2987a576f8b91968
matmak13 <maksimowiczmateusz13@gmail.com>
2021-05-25 22:03:13 +0200

    Zapytanie.cs

---
+++ POProjekt/Zapytanie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POProjekt
+{
+    public class Zapytanie
+    {
+        public string Pytanie;
+        public readonly Osoba Osoba;
+        public readonly Firma Firma;
+        public readonly Bank Bank;
+        public readonly Karta Karta;
+        public readonly DateTime Data;
+        public readonly decimal Kwota;
+
+        public Zapytanie(string pytanie, Osoba osoba, Firma firma, Bank bank, Karta karta, DateTime data, decimal kwota)
+        {
+            Pytanie = pytanie;
+            Osoba = osoba;
+            Firma = firma;
+            Bank = bank;
+            Karta = karta;
+            Data = data;
+            Kwota = kwota;
+        }
+    }
+}
