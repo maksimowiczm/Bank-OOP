049053213d3ca657679431c11b5e640e64fafd66
matmak13 <maksimowiczmateusz13@gmail.com>
2021-05-26 11:59:36 +0200

    Transakcja ToString + TranakcjaJson

--- POProjekt/Transakcja.cs
+++ POProjekt/Transakcja.cs
@@ -30,5 +30,37 @@ namespace POProjekt
         }
         public Transakcja(DateTime data, bool sukces, Bank bank, Firma firma, Osoba osoba, Karta karta, decimal kwota)
             : this(ilosc, data, sukces, bank, firma, osoba, karta, kwota) { }
+
+        public class TransakcjaJson
+        {
+            public int id;
+            public string data;
+            public bool sukces;
+            public decimal kwota;
+            public string bankOsoby;
+            public string osoba;
+            public string bankFirmy;
+            public string firma;
+            public int karta;
+
+            public TransakcjaJson(int id, string data, bool sukces, decimal kwota, Bank bank, Osoba osoba, Firma firma, Karta karta)
+            {
+                this.id = id;
+                this.data = data;
+                this.sukces = sukces;
+                this.kwota = kwota;
+                this.bankOsoby = karta.Bank.ToString();
+                this.osoba = osoba.ToString();
+                bankFirmy = firma.Konta[Firma.DomyslneKonto].Bank.ToString();
+                this.firma = firma.ToString();
+                this.karta = karta.Numer;
+            }
+        }
+        public TransakcjaJson Json() => new(Id, Data.ToString("G"), Sukces, Kwota, Bank, Osoba, Firma, Karta);
+        public override string ToString()
+        {
+            var TrasakcjaJson = new TransakcjaJson(Id, Data.ToString("G"), Sukces, Kwota, Bank, Osoba, Firma, Karta);
+            return JsonConvert.SerializeObject(TrasakcjaJson, Formatting.Indented);
+        }
     }
 }
