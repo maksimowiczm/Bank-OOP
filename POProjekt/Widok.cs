08fab70483708e4e155619f5ea7d3f77e4bca658
TheRavvenKlou <klaudia134000@wp.pl>
2021-05-31 19:20:56 +0200

    widok

---
+++ POProjekt/Widok.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace POProjekt
+{
+    public abstract class Widok
+    {
+        public List<string> lista;
+        protected int wybor;
+        protected Widok(List<string> lista)
+        {
+            this.lista = lista;
+        }
+
+        public virtual void Wyswietl()
+        {
+            for (int i = 1; i <= lista.Count; i++)
+            {
+                Console.WriteLine($"{i}. {lista[i]}");
+            }
+        }
+    }
+}
