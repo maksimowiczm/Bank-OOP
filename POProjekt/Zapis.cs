f20d4b0cb94fc392b0d962f599b0e56e5682f53b
matmak13 <maksimowiczmateusz13@gmail.com>
2021-06-01 21:10:53 +0200

    Zapisz.cs

---
+++ POProjekt/Zapis.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace POProjekt
+{
+    public class Zapis
+    {
+        public List<Kredytowa.KredytowaJson> Kredytowe { get; init; }
+        public List<Debetowa.DebetowaJson> Debetowe { get; init; }
+        public List<Konto.KontoJson> Konta { get; init; }
+        public List<Bank.BankJson> Banki { get; init; }
+        public List<Osoba.OsobaJson> Osoby { get; init; }
+        public List<Firma.FirmaJson> Firmy { get; init; }
+
+        public void Zapisz(string plik)
+        {
+            File.WriteAllText(plik, JsonConvert.SerializeObject(this, Json.JsonSerializerSettings));
+        }
+    }
+}
